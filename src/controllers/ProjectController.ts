c315eea52631a193e10aa7f6fd6ef8b130d94595
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-08 19:36:49 -0300

    feat: Add getAllProjects method to ProjectController and corresponding route

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -13,6 +13,13 @@ export class ProjectController {
     }
   };
   static getAllProjects = async (req: Request, res: Response) => {
+    try {
+        const projects = await Project.find({});
+        res.json(projects);
+
+    } catch (error) {
+        console.log(error)
+    }
     res.send("Todos los proyectos");
   };
 }
