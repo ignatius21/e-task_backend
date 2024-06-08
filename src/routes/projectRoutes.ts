7d81889b2d285dab55e255980e9093536191c16b
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-08 06:47:37 -0300

    feat: Add createProject method to ProjectController and corresponding route

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -4,5 +4,7 @@ import { ProjectController } from '../controllers/ProjectController';
 const router = Router();
 
 router.get('/',ProjectController.getAllProjects);
+router.post('/',ProjectController.createProject);
+
 
 export default router;
