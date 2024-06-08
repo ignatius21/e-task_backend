c4a5cb378150837ce2067f05398c111c3216d151
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-08 06:58:45 -0300

    chore: Update npm dependencies for express-validator to version 7.1.0

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,10 +1,18 @@
 import  {Router} from 'express';
+import {body} from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
 
+router.post('/',
+    body('projectName').notEmpty().withMessage('Project name is required'),
+    body('clientName').notEmpty().withMessage('Client name is required'),
+    body('description').notEmpty().withMessage('Description is required'),
+    handleInputErrors,
+    ProjectController.createProject
+);
 router.get('/',ProjectController.getAllProjects);
-router.post('/',ProjectController.createProject);
 
 
 export default router;
