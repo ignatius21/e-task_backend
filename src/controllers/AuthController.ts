2eebc113f42b3157e02a762210cddaac8f6072a1
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-18 05:58:40 -0300

    feat: Add error handling to createAccount route

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,16 @@
 import type {Request,Response} from 'express';
+import User from '../models/User';
 
 
 
 export class AuthController {
     static createAccount = async (req:Request,res:Response) => {
-        res.send('desde/api/auth');
+        try {
+            const user = await User.create(req.body);
+            await user.save();
+            res.status(201).send('Cuenta creada, revisa tu correo para activarla');
+        } catch (error) {
+            res.status(500).send('Error en el servidor');
+        }
     }
 }
