92aed5e642b1e42ad172d8e5df6a65365b502563
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-18 05:34:56 -0300

    feat: Add route for creating user account

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,22 @@
 import {Router} from 'express';
+import {body} from 'express-validator';
+import { AuthController } from '../controllers/AuthController';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('desde/api/auth');
-});
+router.post('/create-account',
+    body('name').isString().isLength({min:2}).notEmpty().withMessage('El nombre es requerido'),
+    body('email').isEmail(),
+    body('password').isString().isLength({min:6}),
+    body('password_confirmation').custom((value,{req}) => {
+        if(value !== req.body.password){
+            throw new Error('Las contraseñas no coinciden');
+        }
+        return true;
+    }), 
+    handleInputErrors,
+    AuthController.createAccount
+);
 
 export default router;
