de8480493ea12829a68bf4c8b1c9117d9a9b4269
ignatius21 <ignaciojosealberto@hotmail.com>
2024-06-30 21:02:28 -0300

    feat: Agregar controlador de notas para crear una nueva nota en una tarea

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -8,6 +8,8 @@ import { validateProjectExists } from '../middleware/project';
 import { auntenthicate } from '../middleware/auth';
 import { TeamMemberController } from '../controllers/TeamController';
 import { hasAutorization } from '../middleware/task';
+import { NoteController } from '../controllers/NoteCrontroller';
+
 
 const router = Router();
 
@@ -122,5 +124,14 @@ router.delete('/:projectId/team/:userId',
     handleInputErrors,
     TeamMemberController.removeMemberById
 );
+
+// routes for notes
+
+router.post('/:projectId/tasks/:taskId/notes',
+
+    body('content').notEmpty().withMessage('Content is required'),
+    handleInputErrors,
+    NoteController.createNote
+)
 
 export default router;
